5126ebc16a1df3ee6d3072bab875369ebda12af3
bruno08nunes <brunonunes.sl08@gmail.com>
2026-01-31 17:29:13 -0300

    Melhoria da responsividade da página "Sobre Mim"

--- app/sobre-mim/page.tsx
+++ app/sobre-mim/page.tsx
@@ -11,8 +11,8 @@ export default function AboutMePage() {
     return (
         <>
             <Header />
-            <main className="min-w-[320px] w-full mx-auto flex flex-col text-xl py-8">
-                <section className="grid grid-cols-[3fr_2fr] w-full items-center gap-10 min-h-[55svh] max-w-[1300px] mx-auto px-6 py-15">
+            <main className="min-w-[320px] w-full mx-auto flex flex-col md:text-xl text-lg md:py-8">
+                <section className="md:grid grid-cols-[3fr_2fr] flex flex-col-reverse w-full items-center md:gap-10 gap-4 min-h-[55svh] max-w-[1300px] mx-auto px-6 md:py-15 py-4">
                     <div className="flex flex-col gap-4">
                         <h2 className="text-[1.8em] text-balance font-bold">
                             Quem sou eu
@@ -37,15 +37,15 @@ export default function AboutMePage() {
                         alt=""
                         width={400}
                         height={400}
-                        className="max-w-full w-[70%] rounded-full object-cover"
+                        className="max-w-full md:w-[70%] w-[200px] rounded-full object-cover"
                         loading="eager"
                     />
                 </section>
-                <section className="grid grid-cols-[2fr_3fr] w-full items-center gap-10 min-h-[55svh] max-w-[1300px] mx-auto px-6 py-15">
+                <section className="lg:grid grid-cols-[1fr_2fr] flex flex-col w-full items-center md:gap-10 min-h-[55svh] max-w-[1300px] mx-auto px-6 md:py-15 py-2">
                     <Image
                         src={programingImage}
                         alt=""
-                        className="w-full object-cover"
+                        className="max-w-full lg:full w-[300px] md:max-lg:hidden rounded-full object-cover"
                         placeholder="blur"
                     />
                     <div className="flex flex-col gap-4">
@@ -92,7 +92,7 @@ export default function AboutMePage() {
                         </p>
                     </div>
                 </section>
-                <section className="grid grid-cols-[3fr_2fr] w-full items-center gap-10 min-h-[55svh] max-w-[1300px] mx-auto px-6 py-15">
+                <section className="md:grid grid-cols-[3fr_2fr] flex flex-col-reverse w-full items-center md:gap-10 gap-4 min-h-[55svh] max-w-[1300px] mx-auto px-6 md:py-15 py-4">
                     <div className="flex flex-col gap-4">
                         <h2 className="text-[1.8em] text-balance font-bold">
                             O que me motiva
@@ -116,12 +116,12 @@ export default function AboutMePage() {
                     <Image
                         src={programingImage2}
                         alt=""
-                        className="max-w-full w-[70%] object-cover"
+                        className="max-w-full md:w-[70%] w-[300px] object-cover"
                         placeholder="blur"
                     />
                 </section>
                 <section className="bg-[#5E17EB] w-full min-h-[50vh] flex justify-center p-8">
-                    <div className="max-w-[1300px] text-xl flex gap-8 items-center">
+                    <div className="max-w-[1300px] md:text-xl flex gap-8 items-center">
                         <div className="flex flex-col gap-4 text-justify">
                             <h2 className="text-[1.5em] text-balance font-bold mb-2">
                                 O que procuro
@@ -151,16 +151,16 @@ export default function AboutMePage() {
                             alt=""
                             width={200}
                             height={200}
-                            className="max-w-full w-[60%] rounded-full object-cover "
+                            className="max-w-full w-[60%] rounded-full object-cover hidden sm:block"
                         />
                     </div>
                 </section>
-                <section className="grid grid-cols-[3fr_2fr] w-full items-center gap-2 min-h-[55svh] max-w-[1300px] mx-auto px-6 py-15">
+                <section className="grid md:grid-cols-[3fr_2fr] w-full items-center gap-2 min-h-[55svh] max-w-[1300px] mx-auto px-6 py-15">
                     <div className="flex flex-col gap-4">
                         <h2 className="text-[1.8em] text-balance font-bold">
                             Tecnologias e áreas de atuação
                         </h2>
-                        <ul className="flex gap-7">
+                        <ul className="flex gap-7 sm:flex-row flex-col">
                             <li className="flex-1">
                                 <strong>Web Development</strong>
                                 <ul className="ml-5 list-disc">
@@ -202,7 +202,7 @@ export default function AboutMePage() {
                     <Image
                         src={pcImage}
                         alt=""
-                        className="max-w-full w-[70%] rounded-full object-cover"
+                        className="max-w-full w-[70%] rounded-full object-cover hidden md:block"
                         placeholder="blur"
                     />
                 </section>
